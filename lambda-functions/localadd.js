97e79548116f32553cdcce3908fb8f53ad704d58
sumitsingh-ara <testing@192.168.1.4>
2024-04-15 00:08:39 +0530

    Setup done for github workflow deployment file

---
+++ lambda-functions/localadd.js
@@ -0,0 +1,11 @@
+exports.handler = async (event, context) => {
+  const { body } = event;
+
+  const first = body.a || 0;
+  const second = body.b || 0;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`Hello from Lambda! Your Sum is => ${first + second}`),
+  };
+};
